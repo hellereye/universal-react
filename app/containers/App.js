13f94897aead63a322039b370b4823fef7a1422f
fanfree <fanfree@email.com>
2016-06-29 09:21:24 +0800

    modify left nav and css

--- app/containers/App.js
+++ app/containers/App.js
@@ -30,17 +30,6 @@ import {
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import {
-	Card,
-	CardActions,
-	CardHeader,
-	CardMedia,
-	CardTitle,
-	CardText
-} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-
-
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import FontIcon from 'material-ui/FontIcon';
@@ -64,6 +53,8 @@ import ActionAssignment from 'material-ui/svg-icons/action/assignment';
 import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 
+import IconButton from 'material-ui/IconButton';
+
 const style = {
 	margin: 5,
 };
@@ -76,12 +67,7 @@ const muiTheme = getMuiTheme({
 });
 
 injectTapEventPlugin();
-// import { Grid, Row, Col } from 'react-flexbox-grid';
-const {
-	Grid,
-	Row,
-	Col
-} = require('react-flexbox-grid');
+
 
 
 class App extends Component {
@@ -166,8 +152,6 @@ class App extends Component {
 							</ListItem>
 						</List>
 			        </Drawer>
-				
-				
 					<nav>
 						<ul>
 							<li><Link to='/'>Users</Link></li>
@@ -177,87 +161,9 @@ class App extends Component {
 						this.props.children
 					} 
 
-					<Grid>
-					<Row>
-						<Col lg={3}>
-							<Card>
-							    
-							    <CardMedia
-							      overlay={<CardTitle title="雅思英语第二节" subtitle="Overlay subtitle" />}
-							    >
-								<img src="http://lorempixel.com/300/164/nature/" />
-							    </CardMedia>
-							    
-							    <CardText>
-							      最及时考试资讯和预测机经，顶尖教研团队特供真题解析
-							     
-							    </CardText>
-							    <CardActions>
-							      <FlatButton label="进入课堂" />
-							      <RaisedButton label="Primary" primary={true} style={style} />
-							    </CardActions>
-							</Card>
-						</Col>
-						<Col lg={3}>
-							<Card>
-							    
-							    <CardMedia
-							      overlay={<CardTitle title="托福应用一对一培训" subtitle="Overlay subtitle" />}
-							    >
-							      <img src="http://lorempixel.com/300/164/abstract/" />
-							    </CardMedia>
-							    
-							    <CardText>
-							      历年官方真题雅思机经追击，英伦腔明星老师掀屠鸭热潮
-							     
-							    </CardText>
-							    <CardActions>
-							      <FlatButton label="进入课堂" />
-							      <RaisedButton label="Primary" primary={true} style={style} />
-							    </CardActions>
-							</Card>
-						</Col>
-						<Col lg={3}>
-							<Card>
-							    
-							    <CardMedia
-							      overlay={<CardTitle title="托福应用一对一培训" subtitle="Overlay subtitle" />}
-							    >
-							      <img src="http://lorempixel.com/300/164/animals/" />
-							    </CardMedia>
-							    
-							    <CardText>
-							    	考前读机经考后真题看解析，解读改革新政全力备战2016
-							     
-							    </CardText>
-							    <CardActions>
-							      <FlatButton label="进入课堂" />
-							      <RaisedButton label="Primary" primary={true} style={style} />
-							    </CardActions>
-							</Card>
-						</Col>
-						<Col lg={3}>
-							<Card>
-							    
-							    <CardMedia
-							      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-							    >
-							      <img src="http://lorempixel.com/300/164/fashion/" />
-							    </CardMedia>
-							    
-							    <CardText>
-							    	全面攻克写作数学逻辑难关，手捧奖学金踏上研究生之路							     
-							    </CardText>
-							    <CardActions>
-							      <FlatButton label="进入课堂" />
-							      <RaisedButton label="Primary" primary={true} style={style} />
-							    </CardActions>
-							</Card>
-						</Col>
-					</Row>
-				</Grid>
-				
-		</div>
+					
+						
+				</div>
 
 			</MuiThemeProvider>
 		);
--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import Helmet from 'react-helmet';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import {
+	connect
+} from 'react-redux';
+import {
+	Link
+} from 'react-router';
 import * as UsersActions from '../actions/users';
 import UserList from '../components/UserList';
+
+// import { Grid, Row, Col } from 'react-flexbox-grid';
+const {
+	Grid,
+	Row,
+	Col
+} = require('react-flexbox-grid');
+
+import {
+	Card,
+	CardActions,
+	CardHeader,
+	CardMedia,
+	CardTitle,
+	CardText
+} from 'material-ui/Card';
+
+import {
+	RaisedButton
+} from 'material-ui';
+
+import FlatButton from 'material-ui/FlatButton';
+
+const style = {
+	margin: 12,
+};
 
 // @connect(state => { users: state.users })
 class Home extends Component {
@@ -39,6 +71,86 @@ class Home extends Component {
 				<Helmet title='Home' />
 				<h5>Users:</h5>
 				{this.renderUsers()}
+
+				<Grid>
+					<Row>
+						<Col lg={3}>
+							<Card>
+							    
+							    <CardMedia
+							      overlay={<CardTitle title="雅思英语第二节" subtitle="Overlay subtitle" />}
+							    >
+								<img src="http://lorempixel.com/300/164/nature/" />
+							    </CardMedia>
+							    
+							    <CardText>
+							      最及时考试资讯和预测机经，顶尖教研团队特供真题解析
+							     
+							    </CardText>
+							    <CardActions>
+							      <FlatButton label="进入课堂" />
+							      <RaisedButton label="Primary" primary={true} style={style} />
+							    </CardActions>
+							</Card>
+						</Col>
+						<Col lg={3}>
+							<Card>
+							    
+							    <CardMedia
+							      overlay={<CardTitle title="托福应用一对一培训" subtitle="Overlay subtitle" />}
+							    >
+							      <img src="http://lorempixel.com/300/164/abstract/" />
+							    </CardMedia>
+							    
+							    <CardText>
+							      历年官方真题雅思机经追击，英伦腔明星老师掀屠鸭热潮
+							     
+							    </CardText>
+							    <CardActions>
+							      <FlatButton label="进入课堂" />
+							      <RaisedButton label="Primary" primary={true} style={style} />
+							    </CardActions>
+							</Card>
+						</Col>
+						<Col lg={3}>
+							<Card>
+							    
+							    <CardMedia
+							      overlay={<CardTitle title="托福应用一对一培训" subtitle="Overlay subtitle" />}
+							    >
+							      <img src="http://lorempixel.com/300/164/animals/" />
+							    </CardMedia>
+							    
+							    <CardText>
+							    	考前读机经考后真题看解析，解读改革新政全力备战2016
+							     
+							    </CardText>
+							    <CardActions>
+							      <FlatButton label="进入课堂" />
+							      <RaisedButton label="Primary" primary={true} style={style} />
+							    </CardActions>
+							</Card>
+						</Col>
+						<Col lg={3}>
+							<Card>
+							    
+							    <CardMedia
+							      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+							    >
+							      <img src="http://lorempixel.com/300/164/fashion/" />
+							    </CardMedia>
+							    
+							    <CardText>
+							    	全面攻克写作数学逻辑难关，手捧奖学金踏上研究生之路							     
+							    </CardText>
+							    <CardActions>
+							      <FlatButton label="进入课堂" />
+							      <RaisedButton label="Primary" primary={true} style={style} />
+							    </CardActions>
+							</Card>
+						</Col>
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
